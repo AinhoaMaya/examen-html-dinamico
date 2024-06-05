3ac671d2a6b7a8123fc32d65a6f49eb81573a5e4
AinhoaMaya <ainhoamaya.web@gmail.com>
2024-06-05 11:40:04 +0200

    Se modifica la tercera card

--- front/src/components/cards.js
+++ front/src/components/cards.js
@@ -71,8 +71,23 @@ class Cards extends HTMLElement {
           border: 1px solid hsl(0, 1%, 63%);
         }
 
+        .card-special{
+          display: flex;
+          align-items: center;
+          border-radius: 0.4rem;
+          flex-direction: column;
+          max-width: 18rem;
+          border: 1px solid hsl(204, 100%, 47%);
+        }
+
         .card-title{
           background-color: hsl(240, 1%, 34%);
+          max-height: 5rem;
+          min-width: 18rem;
+        }
+
+        .card-title-special{
+          background-color: hsl(204, 100%, 47%);
           max-height: 5rem;
           min-width: 18rem;
         }
@@ -92,6 +107,7 @@ class Cards extends HTMLElement {
 
         span{
           font-size: 2rem;
+          font-weight: 700;
         }
 
         ul{
@@ -110,8 +126,8 @@ class Cards extends HTMLElement {
           border: none;
           border-radius: 0.2rem;
           color: hsl(0, 0%, 100%);
-          font-size: 13px;
-          font-weight: 300;
+          font-size: 14px;
+          font-weight: 600;
           min-width: 15rem;
           outline: none;
           padding: 0.8rem 2.8rem;
@@ -129,53 +145,61 @@ class Cards extends HTMLElement {
     `
     const cards = this.shadow.querySelector('.cards');
 
-    this.data.forEach(product => {
+    this.data.forEach((product, index) => {
       const card = document.createElement('div');
       cards.appendChild(card);
       card.classList.add('card');
-    
+
+      if (index === 2) {
+        card.classList.add('card-special');
+      }
+
       const cardTitleContainer = document.createElement('div');
       card.appendChild(cardTitleContainer);
       cardTitleContainer.classList.add('card-title');
-    
+
+      if (index === 2) {
+        cardTitleContainer.classList.add('card-title-special');
+      }
+
       const cardTitle = document.createElement('h2');
       cardTitleContainer.appendChild(cardTitle);
       cardTitle.textContent = product.title;
-    
+
       const cardContent = document.createElement('div');
       card.appendChild(cardContent);
       cardContent.classList.add('card-content');
-    
+
       const cardPrice = document.createElement('div');
       cardContent.appendChild(cardPrice);
       cardPrice.classList.add('card-price');
-    
+
       const cardPriceSpan = document.createElement('span');
       cardContent.appendChild(cardPriceSpan);
       cardPriceSpan.textContent = product.price;
-    
+
       const cardList = document.createElement('div');
       cardContent.appendChild(cardList);
       cardList.classList.add('card-list');
-    
+
       const cardListUl = document.createElement('ul');
       cardList.appendChild(cardListUl);
-    
+
       product.list.forEach(listItem => {
         const cardListLi = document.createElement('li');
         cardListUl.appendChild(cardListLi);
         cardListLi.textContent = listItem;
         cardListLi.classList.add('li');
       });
-    
+
       const cardButtonContainer = document.createElement('div');
       card.appendChild(cardButtonContainer);
       cardButtonContainer.classList.add('card-button');
-    
+
       const cardButton = document.createElement('button');
       cardButtonContainer.appendChild(cardButton);
       cardButton.textContent = product.buttonText;
-      cardButton.classList.add('buttons')
+      cardButton.classList.add('buttons');
     });
   }
 }
